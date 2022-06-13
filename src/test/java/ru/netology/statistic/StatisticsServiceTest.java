0c3f26bf74e3465a6ecd442f27f085e9b7c5dcbe
Евгения Пискарева <evgen-zykova@mail.ru>
2022-06-13 23:30:19 +0300

    Add to test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -22,11 +22,12 @@ public class StatisticsServiceTest {
     void findEquality()  {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
-        long expected = 10;
+        long[] incomesInBillions = {2, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
+
 }
