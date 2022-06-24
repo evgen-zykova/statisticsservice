5c1c8e885a17e1c917c8e73de63173948e03ae1c
Евгения Пискарева <evgen-zykova@mail.ru>
2022-06-24 19:20:22 +0300

    Add to test3

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,18 +1,48 @@
 package ru.netology.statistic;
 
+
 public class StatisticsService {
-    /**
-     * Calculate index of max income
-     *
-     * @param incomes - array of incomes
-     * @return - index of first max value
-     */
-    public long findMax(long[] incomes) {
-        long current_max_index = 0;
-        long current_max = incomes[0];
-        for (long income : incomes)
-            if (current_max < income)
-                current_max = income;
-        return current_max;
+
+    public int sumAllSales(int[] sales) {
+        int sum = 0;
+        for (int i = 0; i < sales.length; i++) {
+            sum += sales[i];
+        }
+        return sum;
+    }
+
+    public double averageSalesAmount(int[] sales) {
+        double averageSum = 0;
+        int amount = sumAllSales(sales);
+
+        averageSum = amount / sales.length;
+        return averageSum;
+
+    }
+
+    public int monthOfMaximumSales(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
     }
 }
